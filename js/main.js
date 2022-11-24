9e818fe9888169a41cf1edb5fa49a44e4c1d07aa
kimdongzzi <dongzzi2035@gmail.com>
2022-11-24 09:04:47 +0900

    post 저장 수정 삭제 조회 기능 조회는 일단 post라는 페이지를 만들어서 클릭한 글만 보이게 하는 것은 오늘 더 생각을 해야합니다

--- js/main.js
+++ js/main.js
@@ -1,58 +1,58 @@
-import { handleAuth, logout, socialLogin, onToggle } from "./pages/login.js";
-import { handleLocation, goToProfile } from "./router.js";
-import { authService } from "./firebase.js";
+import { handleAuth, logout, socialLogin, onToggle } from './pages/login.js';
+import { handleLocation, goToProfile } from './router.js';
+import { authService } from './firebase.js';
 import {
-    openEditBoxName, closeEditBoxName, changeName, openEditBoxBlog,
-    closeEditBoxBlog, changeBlog, openEditBoxBirth, closeEditBoxBirth,
-    changeBirth, openEditBoxText, closeEditBoxText, changeText
-} from "./pages/profile.js";
-
+  openEditBoxName,
+  closeEditBoxName,
+  changeName,
+  openEditBoxBlog,
+  closeEditBoxBlog,
+  changeBlog,
+  openEditBoxBirth,
+  closeEditBoxBirth,
+  changeBirth,
+  openEditBoxText,
+  closeEditBoxText,
+  changeText,
+} from './pages/profile.js';
+import {
+  savePost,
+  updatePost,
+  onEditing,
+  deletePost,
+  getPostList,
+} from './pages/newPost.js';
 
 // url 바뀌면 handleLocation 실행하여 화면 변경
-window.addEventListener("hashchange", handleLocation);
+window.addEventListener('hashchange', handleLocation);
 
 // 첫 랜딩 또는 새로고침 시 handleLocation 실행하여 화면 변경
-document.addEventListener("DOMContentLoaded", function () {
-    // Firebase 연결상태를 감시
-    authService.onAuthStateChanged((user) => {
-        // Firebase 연결되면 화면 표시
-        handleLocation();
-        const hash = window.location.hash;
-        const postBtn = document.getElementById("newPost");
-        if (user) {
-            // <<<<<<< HEAD
-            //             // 로그인 상태이므로 항상 홈화면으로 이동
-            //             if (hash === "") {
-            //                 // 로그인 상태에서는 로그인 화면으로 되돌아갈 수 없게 설정
-            //                 window.location.replace("#home");
-            // =======
-            console.log("로그인")
-            postBtn.style.display = 'block';
-            if (hash === "#login") {
-                window.location.replace("");
-            }
-        } else {
-            console.log("로그아웃")
-            postBtn.style.display = 'none';
-            // 로그아웃 상태이므로 로그인 화면으로 강제 이동
-            // if (hash !== "") {
-            //     window.location.replace("");
-            // }
-        }
-    });
-});
-// 드롭다운 버튼 외부의 영역 클릭 시 사라지게
-window.addEventListener('mousedown', function (event) {
-    const dropdown1 = this.document.querySelector('.navbarUserAccountMenu');
-    const dropdown2 = this.document.querySelector('.navbarBeforeLogin');
-    if (!dropdown1.contains(event.target)) {
-        dropdown1.classList.remove('active')
+document.addEventListener('DOMContentLoaded', function () {
+  // Firebase 연결상태를 감시
+  authService.onAuthStateChanged((user) => {
+    // Firebase 연결되면 화면 표시
+    handleLocation();
+    const hash = window.location.hash;
+    if (user) {
+      // <<<<<<< HEAD
+      //             // 로그인 상태이므로 항상 홈화면으로 이동
+      //             if (hash === "") {
+      //                 // 로그인 상태에서는 로그인 화면으로 되돌아갈 수 없게 설정
+      //                 window.location.replace("#home");
+      // =======
+      console.log('로그인');
+      if (hash === '#login') {
+        window.location.replace('#feed');
+      }
+    } else {
+      console.log('로그아웃');
+      // 로그아웃 상태이므로 로그인 화면으로 강제 이동
+      // if (hash !== "") {
+      //     window.location.replace("");
+      // }
     }
-    if (!dropdown2.contains(event.target)) {
-        dropdown2.classList.remove('active')
-    }
+  });
 });
-
 // type=module, 모듈객체는 지역적으로 밖에 사용 불가. 따라서 윈도우객체에 할당함으로써 전역적으로 사용 가능해짐
 // onclick, onchange, onsubmit 이벤트 핸들러 리스트
 
@@ -63,7 +63,8 @@ window.logout = logout;
 window.onToggle = onToggle;
 // window.onFileChange = onFileChange;
 // window.changeProfile = changeProfile;
-// window.save_comment = save_comment;
-// window.update_comment = update_comment;
-// window.onEditing = onEditing;
-// window.delete_comment = delete_comment;
+window.savePost = savePost;
+window.updatePost = updatePost;
+window.onEditing = onEditing;
+window.deletePost = deletePost;
+window.getPostList = getPostList;
--- js/pages/newPost.js
+++ js/pages/newPost.js
@@ -1,27 +1,142 @@
 import {
-    addDoc,
-    collection,
-  } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js";
-  import { dbService, authService } from "../firebase.js";
-  
-  export const savePost = async (event) => {
-    event.preventDefault();
-    const post = document.getElementById("post");
-    const { uid, photoURL, displayName } = authService.currentUser;
+  doc,
+  addDoc,
+  updateDoc,
+  deleteDoc,
+  collection,
+  orderBy,
+  query,
+  getDocs,
+} from 'https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js';
+import { dbService, authService } from '../firebase.js';
+
+export const savePost = async (event) => {
+  event.preventDefault();
+  const post = document.getElementById('writePost');
+  const postTitle = document.getElementById('writeTitle');
+  const { uid, photoURL, displayName } = authService.currentUser;
+  try {
+    await addDoc(collection(dbService, 'posts'), {
+      title: postTitle.value,
+      text: post.value.replace(/(\n|\r\n)/g, '<br>').replace(/ /g, '&nbsp'),
+      createdAt: Date.now(),
+      creatorId: uid,
+      profileImg: photoURL,
+      nickname: displayName,
+    });
+    post.value = '';
+    getPostList();
+  } catch (error) {
+    alert(error);
+    console.log('error in addDoc:', error);
+  }
+};
+
+export const onEditing = (event) => {
+  // 수정버튼 클릭
+  event.preventDefault();
+  const udBtns = document.querySelectorAll('.postEditBtn, .postDeleteBtn');
+  udBtns.forEach((udBtn) => (udBtn.disabled = 'true'));
+
+  const postBody = event.target.parentNode.parentNode.parentNode;
+  const postText = postBody.children[1].children[0];
+  const postInputP = postBody.children[1].children[1];
+
+  postText.classList.add('noDisplay');
+  postInputP.classList.add('yesDisplay');
+  postInputP.classList.remove('noDisplay');
+  postInputP.children[0].focus();
+};
+
+export const updatePost = async (event) => {
+  event.preventDefault();
+  const newPost = event.target.parentNode.children[0].value
+    .replace(/(\n|\r\n)/g, '<br>')
+    .replace(/ /g, '&nbsp');
+  const id = event.target.parentNode.id;
+
+  const parentNode = event.target.parentNode.parentNode;
+  const postText = parentNode.children[0];
+  postText.classList.remove('noDisplay');
+  const postInputP = parentNode.children[1];
+  postInputP.classList.remove('yesDisplay');
+  postInputP.classList.add('noDisplay');
+
+  const postRef = doc(dbService, 'posts', id);
+  try {
+    await updateDoc(postRef, { text: newPost });
+    getPostList();
+  } catch (error) {
+    alert(error);
+  }
+};
+
+export const deletePost = async (event) => {
+  event.preventDefault();
+  const id = event.target.name;
+  const ok = window.confirm('해당 글을 정말 삭제하시겠습니까?');
+  if (ok) {
     try {
-      await addDoc(collection(dbService, "posts"), {
-        text: post.value,
-        createdAt: Date.now(),
-        creatorId: uid,
-        profileImg: photoURL,
-        nickname: displayName,
-      });
-      post.value = "";
-      getpostList();
+      await deleteDoc(doc(dbService, 'posts', id));
+      getPostList();
     } catch (error) {
       alert(error);
-      console.log("error in addDoc:", error);
     }
-  };
-  
+  }
+};
 
+export const getPostList = async () => {
+  let postObjList = [];
+  const q = query(collection(dbService, 'posts'), orderBy('createdAt', 'desc'));
+  const querySnapshot = await getDocs(q);
+  querySnapshot.forEach((doc) => {
+    const postObj = {
+      id: doc.id,
+      ...doc.data(),
+    };
+    console.log(postObj);
+    postObjList.push(postObj);
+  });
+  const postList = document.getElementById('postList');
+  const currentUid = authService.currentUser.uid;
+  postList.innerHTML = '';
+  postObjList.forEach((postObj) => {
+    const isOwner = currentUid === postObj.creatorId;
+    const temp_html = `<div class="postWrap">
+                        <div class="postTitle">
+                          <h2>${postObj.title}</h2>
+                        </div>
+                        <div class="postDesc">
+                          <p>
+                            ${postObj.text}
+                          </p>
+                          <p id="${postObj.id}" class="noDisplay">
+                                <textarea class="writePost" placeholder="제목" id="post"></textarea><button class="updateBtn" onclick="updatePost(event)">완료</button></p>
+                          <span class="postDate">${new Date(postObj.createdAt)
+                            .toString()
+                            .slice(0, 25)}</span>
+                        </div>
+                        <div class="postProfile">
+                          <img class="cmtImg profileImg" width="50px" height="50px" src="${
+                            postObj.profileImg
+                          }" alt="profileImg" />
+                          <span class="profileName">${
+                            postObj.nickname ?? '닉네임 없음'
+                          }</span>
+                          <div class="postChangeGroup ${
+                            isOwner ? 'updateBtns' : 'noDisplay'
+                          }">
+                            <button class="postEditBtn postBtn" onclick="onEditing(event)">수정</button>
+                            <button name="${
+                              postObj.id
+                            }" class="postDeleteBtn postBtn" onclick="deletePost(event)">삭제</button>
+                          </div>
+                        </div>
+                      </div>`;
+
+    const div = document.createElement('div');
+    div.classList.add('myPost');
+    div.innerHTML = temp_html;
+    postList.appendChild(div);
+  });
+};
